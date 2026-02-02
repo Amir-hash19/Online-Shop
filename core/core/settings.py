c2b2e91963f1aac3ff0349a9287eabf9af0a1f55
Amirhoseinheidari <amirhosein.hydri1381@gmail.com>
2026-02-02 22:37:48 +0330

    implemented HTML templates and static files :green_heart:

--- core/core/settings.py
+++ core/core/settings.py
@@ -122,17 +122,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
 
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
-STATIC_ROOT = BASE_DIR / "media"
+STATIC_URL = '/static/'  
+MEDIA_URL = '/media/'   
+
+STATIC_ROOT = BASE_DIR / "staticfiles"  
+MEDIA_ROOT = BASE_DIR / "media"        
 
 
-STATIC_AUTO_DIRS = [
-    BASE_DIR / "static"
+
+STATICFILES_DIRS = [     
+    BASE_DIR / "static",   
 ]
+
+
+
 
 
 
