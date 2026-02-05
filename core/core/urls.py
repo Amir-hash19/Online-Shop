ac635c0b5d0ae79e7e299e56347d084950242ba0
Amirhoseinheidari <amirhosein.hydri1381@gmail.com>
2026-02-06 01:39:31 +0330

    implemented login form

--- core/core/urls.py
+++ core/core/urls.py
@@ -5,7 +5,11 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('website.urls'))
+    path('',include('website.urls')),
+    path('accounts/', include('account.urls')),
+
+    
+   
 ]
 
 
