bdb30ef4fdd1913ff43fba50075f01c7af35e893
Amirhoseinheidari <amirhosein.hydri1381@gmail.com>
2026-02-10 00:07:56 +0330

    implemented pagination

--- core/shop/views.py
+++ core/shop/views.py
@@ -1,12 +1,21 @@
 from django.views.generic import TemplateView, ListView, DetailView
 from .models import Product, ProductStatusType
 
+
+
 class ShopProductGridView(ListView):
     template_name = "shop/product-grid.html"
-    queryset = Product.objects.filter(status=ProductStatusType.publish.value)
+    model = Product
+    paginate_by = 9
+
+    def get_queryset(self):
+        return Product.objects.filter(
+            status=ProductStatusType.publish.value
+        ).order_by('-id')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["total_items"] = self.get_queryset().count()
         return context
+
 
