a476f0f5e852493f71dfe9bd09bc86b6dd789290
Amirhoseinheidari <amirhosein.hydri1381@gmail.com>
2026-02-25 13:04:32 +0330

    add Session add product view and implemented context processor

--- core/account/phone_number_validator.py
+++ core/account/phone_number_validator.py
@@ -7,3 +7,4 @@ def validate_iranian_cellphone_number(value):
     pattern = r"^09\d{9}$"
     if not re.match(pattern, value):
         raise ValidationError("Enter a valid Iranian cellphone number.")
+ 
--- core/cart/views.py
+++ core/cart/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views.generic import View
+from django.http import JsonResponse
+from .context_processors import CartSession
 
-# Create your views here.
+
+
+class SessionAddProduct(View):
+    
+    def post(self, request, *args, **kwargs):
+        cart = CartSession(request.session)
+
+        return JsonResponse({"cart":cart})
+    
+    
